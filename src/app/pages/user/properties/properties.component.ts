5c07904ef93fdc5830d1befa50ab58ffaf637ac0
Roman <knms1b22.stoliar@kpnu.edu.ua>
2025-01-23 15:19:20 +0200

    Renamed modules

---
+++ src/app/pages/user/properties/properties.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+	templateUrl: './properties.component.html',
+	styleUrls: ['./properties.component.scss'],
+	standalone: false
+})
+export class PropertiesComponent {}
