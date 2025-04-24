f7dcd3c1afb2cf323016380de3fd1964113ef5ce
Nastia <nastia7408@gmail.com>
2025-04-24 15:32:22 +0300

    day 2

--- src/app/modules/propertytask/services/propertytask.service.ts
+++ src/app/modules/propertytask/services/propertytask.service.ts
@@ -1,14 +1,19 @@
 import { Injectable } from '@angular/core';
 import { Propertytask } from '../interfaces/propertytask.interface';
-import { CrudService } from 'wacom';
+import { AlertService, CoreService, CrudService, HttpService, StoreService } from 'wacom';
 
 @Injectable({
 	providedIn: 'root',
 })
 export class PropertytaskService extends CrudService<Propertytask> {
-	constructor() {
+	constructor(_http: HttpService, _store: StoreService, _alert: AlertService, _core: CoreService) {
 		super({
-			name: 'propertytask',
-		});
+			name: 'propertytask'
+		},
+			_http,
+			_store,
+			_alert,
+			_core
+		);
 	}
 }
