3e1a85e607b59dd5342a03e310635c5151020dc8
Nastia <nastia7408@gmail.com>
2025-05-01 12:01:39 +0300

    merge

--- src/app/pages/user/materials/materials.component.ts
+++ src/app/pages/user/materials/materials.component.ts
@@ -1,19 +1,31 @@
 import { Component } from '@angular/core';
+import { FormService } from 'src/app/core/modules/form/form.service';
+import { FormInterface } from 'src/app/core/modules/form/interfaces/form.interface';
 
 @Component({
-	templateUrl: './materials.component.html',
-	styleUrls: ['./materials.component.scss'],
-	standalone: false
+  templateUrl: './materials.component.html',
+  styleUrls: ['./materials.component.scss'],
+  standalone: false
 })
 export class MaterialsComponent {
-	isMenuOpen=false;
-}
-create(): void {
-	this._form.modal(this.formDoc, {
-		label: 'Create',
-		click: (data, close) => {
-			console.log('Submitted data:', data);
-			close();
-		},
-	});
+  isMenuOpen = false;
+
+  formDoc: FormInterface | undefined;
+
+  constructor(private _form: FormService) {}
+
+  create(): void {
+    if (!this.formDoc) {
+      console.warn('formDoc не визначено');
+      return;
+    }
+
+    this._form.modal(this.formDoc, {
+      label: 'Create',
+      click: (data: any, close: () => void) => {
+        console.log('Submitted data:', data);
+        close();
+      },
+    });
+  }
 }
