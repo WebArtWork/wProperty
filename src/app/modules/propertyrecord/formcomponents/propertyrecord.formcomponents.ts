b22627a6bdd40649be1ab5d364691282f4a30cfd
Nastia <nastia7408@gmail.com>
2025-05-07 13:33:20 +0300

    update record

--- src/app/modules/propertyrecord/formcomponents/propertyrecord.formcomponents.ts
+++ src/app/modules/propertyrecord/formcomponents/propertyrecord.formcomponents.ts
@@ -3,6 +3,26 @@ export const propertyrecordFormComponents = {
 	title: 'Propertyrecord',
 	components: [
 		{
+			"name": "Select",
+			"key": "type",
+			"focused": true,
+			"fields": [
+			  {
+				"name": "Placeholder",
+				"value": "Select record type"
+			  },
+			  {
+				"name": "Items",
+				"value": [
+				 'Utility bill','Change owner', 'Service', 'Materials', 'Rent bill', 'Incident'
+				]
+			  }
+			]
+		  },
+		
+		
+		
+		/*{
 			"name": "Text",
 			"key": "name",
 			"focused": true,
@@ -17,27 +37,7 @@ export const propertyrecordFormComponents = {
 			  }
 			]
 		  },
-		  {
-			"name": "Select",
-			"key": "type",
-			"focused": true,
-			"fields": [
-			  {
-				"name": "Placeholder",
-				"value": "Select record type"
-			  },
-			  {
-				"name": "Items",
-				"value": [
-				  "sell / buy payment",
-				  "service",
-				  "materials",
-				  "rent payment",
-				  "incident"
-				]
-			  }
-			]
-		  },
+		  
 		  {
 			"name": "Text",
 			"key": "description",
@@ -66,20 +66,7 @@ export const propertyrecordFormComponents = {
 			  }
 			]
 		  },
-		  {
-			"name": "Number",
-			"key": "expenses",
-			"fields": [
-			  {
-				"name": "Placeholder",
-				"value": "Enter expense amount"
-			  },
-			  {
-				"name": "Label",
-				"value": "Expenses"
-			  }
-			]
-		  },
+		 
 		  {
 			"name": "Text",
 			"key": "comments",
@@ -93,7 +80,141 @@ export const propertyrecordFormComponents = {
 				"value": "Comments"
 			  }
 			]
+		  },*/
+		  {
+			"name": "Text",
+			"key": "materials",
+			focused: true,
+			"fields": [
+			  {
+				"name": "Placeholder",
+				"value": "Enter materials information"
+			  },
+			  {
+				"name": "Label",
+				"value": "Materials"
+			  }
+			]
+		  },
+		  {
+			"name": "Text",
+			"key": "photos",
+			
+			"fields": [
+			  {
+				"name": "Placeholder",
+				"value": "Enter photo URLs or upload"
+			  },
+			  {
+				"name": "Label",
+				"value": "Photos"
+			  }
+			]
+		  },
+		  {
+			"name": "Text",
+			"key": "worker",
+			
+			"fields": [
+			  {
+				"name": "Placeholder",
+				"value": "Enter worker id"
+			  },
+			  {
+				"name": "Label",
+				"value": "Worker"
+			  }
+			]
+		  },{
+			"name": "Text",
+			"key": "seller",
+			
+			"fields": [
+			  {
+				"name": "Placeholder",
+				"value": "Enter seller id"
+			  },
+			  {
+				"name": "Label",
+				"value": "Seller"
+			  }
+			]
+		  },{
+			"name": "Text",
+			"key": "buyer",
+			
+			"fields": [
+			  {
+				"name": "Placeholder",
+				"value": "Enter buyer id"
+			  },
+			  {
+				"name": "Label",
+				"value": "Buyer"
+			  }
+			]
+		  },
+		  {
+			"name": "Text",
+			"key": "service",
+			
+			"fields": [
+			  {
+				"name": "Placeholder",
+				"value": "Enter service details"
+			  },
+			  {
+				"name": "Label",
+				"value": "Service"
+			  }
+			]
+		  },
+		
+		  {
+			"name": "Number",
+			"key": "price",
+			
+			"fields": [
+			  {
+				"name": "Placeholder",
+				"value": "Enter price amount"
+			  },
+			  {
+				"name": "Label",
+				"value": "Price"
+			  }
+			]
+		  },
+		  {
+			"name": "Text",
+			"key": "incident",
+			
+			"fields": [
+			  {
+				"name": "Placeholder",
+				"value": "Enter incident details"
+			  },
+			  {
+				"name": "Label",
+				"value": "Incident"
+			  }
+			]
+		  },
+		  {
+			"name": "Text",
+			"key": "rent",
+			"fields": [
+			  {
+				"name": "Placeholder",
+				"value": "Enter rent details"
+			  },
+			  {
+				"name": "Label",
+				"value": "Rent"
+			  }
+			]
 		  }
+		  
 
 		
 	]
