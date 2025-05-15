8172ff1ac1c5bb367c9f484efbaac226e44db772
Nika <danzi6386@gmail.com>
2025-05-15 12:14:45 +0300

    added users by role on user service

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -8,5 +8,10 @@ export const environment = {
 		description:
 			'An amazing solution to build web or mobile app for your business',
 		icon: 'https://webart.work/api/user/cdn/waw-logo.png'
+	},
+	user: {
+		email: '',
+		password: '',
+		resetPin: null
 	}
 };
