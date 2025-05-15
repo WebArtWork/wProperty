ae7b12059df2ccafeebe3b41725a9a6dda500596
Nika <danzi6386@gmail.com>
2025-05-15 12:36:51 +0300

    reordered theme of user

--- src/app/core/theme/user/user.component.ts
+++ src/app/core/theme/user/user.component.ts
@@ -5,16 +5,19 @@ import { Platform } from '@angular/cdk/platform';
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-user',
-    templateUrl: './user.component.html',
-    styleUrls: ['./user.component.scss'],
-    animations: [coreAnimation],
-    standalone: false
+	selector: 'app-user',
+	templateUrl: './user.component.html',
+	styleUrls: ['./user.component.scss'],
+	animations: [coreAnimation],
+	standalone: false
 })
 export class UserComponent {
 	readonly url = environment.url;
+
 	forceAvatarUrl = '';
+
 	showSidebar = false;
+
 	hideSidebar(): void {
 		if (!this._platform.ANDROID && !this._platform.IOS) {
 			this.showSidebar = false;
